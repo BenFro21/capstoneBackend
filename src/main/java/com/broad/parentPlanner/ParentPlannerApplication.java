9b58388a41362de066a05af08760449e39706a6f
benfro21 <broad0601@gmail.com>
2023-07-05 12:12:27 -0400

    first commit

---
+++ src/main/java/com/broad/parentPlanner/ParentPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.broad.parentPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParentPlannerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/broad/parentPlanner/entities/User.java
@@ -0,0 +1,66 @@
+package com.broad.parentPlanner.entities;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    //Constructors
+    public User() {
+    }
+
+    public User(Long id, String email, String username, String password) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+//    Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
