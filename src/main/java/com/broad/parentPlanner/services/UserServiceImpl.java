29a9b63d7205f38c54eb0fb984b5057f3eb3f51c
benfro21 <broad0601@gmail.com>
2023-07-07 17:04:14 -0400

    added some

--- src/main/java/com/broad/parentPlanner/services/UserServiceImpl.java
+++ src/main/java/com/broad/parentPlanner/services/UserServiceImpl.java
@@ -1,9 +1,16 @@
 package com.broad.parentPlanner.services;
 
+import com.broad.parentPlanner.dtos.UserDto;
+import com.broad.parentPlanner.entities.User;
 import com.broad.parentPlanner.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl {
@@ -13,4 +20,19 @@ public class UserServiceImpl {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public List<String> addUser(UserDto userDto){
+        List<String> response = new ArrayList<>();
+        User user = new User(userDto);
+        userRepository.saveAndFlush(user);
+        response.add("User Added scuccessfully");
+        return response;
+    }
+
+    // doing email instead of username
+    public List<String> userLogin(UserDto userDto){
+        List<String> response = new ArrayList<>();
+        Optional<User> userOptional = userRepository.findByEmail(userDto.getEmail());
+    }
 }
